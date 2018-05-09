78d2861d8f870d74979c437f29a20a956f8b79f9
Sayardiss <sayardiss@localhost.localdomain>
2018-05-10 00:45:09 +0200

    Ajout de l'upload automatique de l'image

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -30,7 +30,7 @@ ConvertMP3320() {
   SRC="$1"
   DEST="$2"
   shopt -s globstar
-  for file in "$SRC"/**/*.flac
+  for file in "$SRC"/**/*.m4a
   do
     filename=$( ExtractFilename "$file" )
     ffmpeg -i "$file" $ARG_MP3_320 "$DEST"/"$filename.mp3"   # Conversion en MP3 v0
@@ -41,7 +41,7 @@ ConvertAAC256() {
   SRC="$1"
   DEST="$2"
   shopt -s globstar
-  for file in "$SRC"/**/*.flac
+  for file in "$SRC"/**/*.m4a
   do
     filename=$( ExtractFilename "$file" )
     ffmpeg -i "$file" $ARG_AAC_256 "$DEST"/"$filename.m4a"   # Conversion en AAC
@@ -52,7 +52,7 @@ ConvertFLAC() {
   SRC="$1"
   DEST="$2"
   shopt -s globstar
-  for file in "$SRC"/**/*.flac
+  for file in "$SRC"/**/*.m4a
   do
     filename=$( ExtractFilename "$file" )
     ffmpeg -i "$file" $ARG_FLAC "$DEST"/"$filename.flac"   # Conversion en FLAC
@@ -69,7 +69,8 @@ ConvertFLAC() {
   FILE=`find "$DOSSIER_SOURCE" -type f -print -quit`
   echo "$FILE a été selectionné pour la saisie facilitée"
 
-  ARTISTE=`mediainfo --Inform="General;%Performer%" "$FILE"`
+  ARTISTE=`mediainfo --Inform="General;%Performer%" "$FILE"
+  LINK_SIGNATURE="rformer%" "$FILE"`
   read -e -p "Artiste > " -i "$ARTISTE" ARTISTE
 
   ALBUM=`mediainfo --Inform="General;%Album%" "$FILE"`
@@ -82,7 +83,8 @@ ConvertFLAC() {
   GENRE=`mediainfo --Inform="General;%Genre%" "$FILE"`
   read -e -p "Genre > " -i "$GENRE" GENRE
 
-  read -p "Lien cover > " COVER
+  ffmpeg -n -i "$FILE" "$OUTPUTDIR/cover.jpg" && $LINKCOVER=`$SCRIPTDIR/imgur.sh` || echo "L'extraction et/ou l'envoi automatique de la cover a échoué..."
+  read -e -p "Lien cover > " -i "$LINKCOVER" LINKCOVER
 
   echo -n "Description de l'album (finir par \$) > "
   read -d '$' DESCRIPTION_ALBUM
@@ -105,7 +107,7 @@ ConvertFLAC() {
   mkdir "$DOSSIER_FLAC"
 
 # Pour chaque fichier .m4a dans le dossier source
-ConvertMP3320 "$DOSSIER_SOURCE" "$DOSSIER_MP3"
+ConvertMP320 "$DOSSIER_SOURCE" "$DOSSIER_MP3"
 ConvertAAC256 "$DOSSIER_SOURCE" "$DOSSIER_AAC"
 ConvertFLAC "$DOSSIER_SOURCE" "$DOSSIER_FLAC"
 
@@ -123,15 +125,16 @@ cp "$DOSSIER_MP3"/mediainfo.nfo NFO_"$ALBUM"_MP3.nfo
 cp "$DOSSIER_FLAC"/mediainfo.nfo NFO_"$ALBUM"_FLAC.nfo
 
 # Copier la cover dans chaque dossier
-ffmpeg -n -i "$file" "$DOSSIER_AAC"/cover.jpg
-ffmpeg -n -i "$file" "$DOSSIER_MP3"/cover.jpg
-ffmpeg -n -i "$file" "$DOSSIER_FLAC"/cover.jpg
+cp "$OUTPUTDIR"/cover.jpg "$DOSSIER_AAC"/cover.jpg
+cp "$OUTPUTDIR"/cover.jpg "$DOSSIER_MP3"/cover.jpg
+mv "$OUTPUTDIR"/cover.jpg "$DOSSIER_FLAC"/cover.jpg
 
 
 # Création du fichier torrent
-mktorrent -v -p -a http://t411.download -o "$DOSSIER_AAC.torrent" "$DOSSIER_AAC"
-mktorrent -v -p -a http://t411.download -o "$DOSSIER_MP3.torrent" "$DOSSIER_MP3"
-mktorrent -v -p -a http://t411.download -o "$DOSSIER_FLAC.torrent" "$DOSSIER_FLAC"
+# TODO
+#  mktorrent -v -p -a http://t411.download -o "$DOSSIER_AAC.torrent" "$DOSSIER_AAC"
+#  mktorrent -v -p -a http://t411.download -o "$DOSSIER_MP3.torrent" "$DOSSIER_MP3"
+#  mktorrent -v -p -a http://t411.download -o "$DOSSIER_FLAC.torrent" "$DOSSIER_FLAC"
 
 
 # Récupérer la taille des dossiers, pour la présentation
@@ -140,8 +143,16 @@ SIZE_MP3=$(du -sm "$DOSSIER_MP3" | awk '{print $1;}')
 SIZE_FLAC=$(du -sm "$DOSSIER_FLAC" | awk '{print $1;}')
 
 # Création des fichiers de présentation
+PROFILELINK="https://www.t411.li/users/torrents/?id=98179255"
+LINK_SIGNATURE="https://s25.postimg.org/ge85a664v/T411Signature.png"
+LINK_DESCRIPTIF2="https://s25.postimg.org/gkh8zipvz/Descriptif2.png"
+LINK_INFORMATION2="https://s25.postimg.org/oh6blhqq7/Information_2.png"
+LINK_DETAILUPLOAD="https://s25.postimg.org/ypu7dkpe7/Details_upload.png"
+LINK_UPLOADERS="https://s25.postimg.org/6zrlp8jv3/Uploaders.jpg"
+
+
 echo "[center]
-[url=https://www.t411.li/users/torrents/?id=98179255][img width=500]https://s25.postimg.org/ge85a664v/T411Signature.png[/img][/url]
+[url=$PROFILELINK][img width=500]$LINK_SIGNATURE[/img][/url]
 
 [b][color=#0FBCA9][size=7].·[ [color=#ff9204]$ARTISTE[/color] $ALBUM ]·.[/size][/color][/b]
 
@@ -150,13 +161,13 @@ echo "[center]
 
 
 
-[url=https://www.t411.li/users/torrents/?id=98179255][img width=400]https://s25.postimg.org/gkh8zipvz/Descriptif2.png[/img][/url]
+[url=$PROFILELINK][img width=400]$LINK_DESCRIPTIF2[/img][/url]
 
 $DESCRIPTION_ALBUM
 
 $DESCRIPTION_ARTISTE
 
-[url=https://www.t411.li/users/torrents/?id=98179255][img width=400]https://s25.postimg.org/oh6blhqq7/Information_2.png[/img][/url]
+[url=$PROFILELINK][img width=400]$LINK_INFORMATION2[/img][/url]
 
 
 [b]Artiste de l'album :[/b] $ARTISTE
@@ -165,7 +176,7 @@ $DESCRIPTION_ARTISTE
 [b]Date de sortie :[/b] $DATE
 
 
-[url=https://www.t411.li/users/torrents/?id=98179255][img width=400]https://s25.postimg.org/ypu7dkpe7/Details_upload.png[/img][/url]
+[url=$PROFILELINK][img width=400]$LINK_DETAILUPLOAD[/img][/url]
 
 " > debut_prez.tmp
 cp debut_prez.tmp "$ARTISTE - $ALBUM ($DATE - AAC 256Kbps).txt"
@@ -188,13 +199,12 @@ echo "[b]Format : [/b] FLAC - Lossless
 [b]Taille des fichiers :[/b] $SIZE_FLAC Mo" >> "$ARTISTE - $ALBUM ($DATE - FLAC).txt"
 
 
-
 echo "
 
-[url=https://www.t411.li/users/torrents/?id=98179255]Allez faire un tour sur mes autres uploads ![/url]
-[url=https://www.t411.li/users/torrents/?id=98179255][img width=300]https://s25.postimg.org/ge85a664v/T411Signature.png[/img][/url]
+[url=$PROFILELINK]Allez faire un tour sur mes autres uploads ![/url]
+[url=$PROFILELINK][img width=300]$LINK_SIGNATURE[/img][/url]
 
-[url=https://www.t411.li/users/torrents/?id=98179255][img width=500]https://s25.postimg.org/6zrlp8jv3/Uploaders.jpg[/img][/url]
+[url=$PROFILELINK][img width=500]$LINK_UPLOADERS[/img][/url]
 
 
 [/center]" > fin_prez.tmp
@@ -203,115 +213,6 @@ cat fin_prez.tmp >> "$ARTISTE - $ALBUM ($DATE - AAC 256Kbps).txt"
 cat fin_prez.tmp >> "$ARTISTE - $ALBUM ($DATE - MP3 320Kbps).txt"
 cat fin_prez.tmp >> "$ARTISTE - $ALBUM ($DATE - FLAC).txt"
 rm fin_prez.tmp
-
-# # Création des fichiers de présentation
-#
-#
-# # Description pour AAC 256 Kbps
-# echo "
-# [center][b][color=#FF5D00][size=4].·[ $ALBUM ]·.[/size][/color][/b]
-#
-# [img width=500]$COVER[/img]
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/detail_album.png[/img]
-#
-# $DESCRIPTION_ALBUM
-#
-# $DESCRIPTION_ARTISTE
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/info_album.png[/img]
-#
-#
-# [b]Artiste de l'album :[/b] $ARTISTE
-# [b]Nombre de disques :[/b] $NB_DISQUES
-# [b]Genre :[/b] $GENRE
-# [b]Date de sortie :[/b] $DATE
-#
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/up_album.png[/img]
-#
-# [b]Format : [/b] AAC 256 Kbps
-# [b]Taille des fichiers :[/b] $SIZE_AAC Mo
-#
-# [url=https://www.t411.li/users/torrents/?id=98179255]Allez faire un tour sur mes autres uploads ![/url]
-#
-#
-# [/center]
-# " > "$ARTISTE - $ALBUM ($DATE - AAC 256Kbps).txt"
-#
-#
-#
-#
-#
-# # Description pour MP3 v0 Kbps
-# echo "
-# [center][b][color=#FF5D00][size=4].·[ $ALBUM ]·.[/size][/color][/b]
-#
-# [img width=500]$COVER[/img]
-#
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/detail_album.png[/img]
-#
-# $DESCRIPTION_ALBUM
-#
-# $DESCRIPTION_ARTISTE
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/info_album.png[/img]
-#
-#
-# [b]Artiste de l'album :[/b] $ARTISTE
-# [b]Nombre de disques :[/b] $NB_DISQUES
-# [b]Genre :[/b] $GENRE
-# [b]Date de sortie :[/b] $DATE
-#
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/up_album.png[/img]
-#
-# [b]Format : [/b] MP3 320 Kbps
-# [b]Taille des fichiers :[/b] $SIZE_MP3 Mo
-#
-# [url=https://www.t411.li/users/torrents/?id=98179255]Allez faire un tour sur mes autres uploads ![/url]
-#
-#
-# [/center]
-# " > "$ARTISTE - $ALBUM ($DATE - MP3 320Kbps).txt"
-#
-#
-#
-#
-#
-# # Description pour FLAC
-# echo "
-# [center][b][color=#FF5D00][size=4].·[ $ALBUM ]·.[/size][/color][/b]
-#
-# [img width=500]$COVER[/img]
-#
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/detail_album.png[/img]
-#
-# $DESCRIPTION_ALBUM
-#
-# $DESCRIPTION_ARTISTE
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/info_album.png[/img]
-#
-#
-# [b]Artiste de l'album :[/b] $ARTISTE
-# [b]Nombre de disques :[/b] $NB_DISQUES
-# [b]Genre :[/b] $GENRE
-# [b]Date de sortie :[/b] $DATE
-#
-#
-# [img width=500]http://www.ledrakkarnoir.com/prez/img_cat/blanc/up_album.png[/img]
-#
-# [b]Format : [/b] FLAC - Lossless
-# [b]Taille des fichiers :[/b] $SIZE_FLAC Mo
-#
-# [url=https://www.t411.li/users/torrents/?id=98179255]Allez faire un tour sur mes autres uploads ![/url]
-#
-#
-# [/center]
-# " > "$ARTISTE - $ALBUM ($DATE - FLAC).txt"
 
 
 
